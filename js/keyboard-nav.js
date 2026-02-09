86e785a4b070dd37b6b323077a1aed0751c32640
haritha-j <haritha.16@cse.mrt.ac.lk>
2026-02-09 11:21:13 +0000

    doodles and resume

---
+++ js/keyboard-nav.js
@@ -0,0 +1,22 @@
+// Keyboard navigation shortcuts
+document.addEventListener('keydown', function(e) {
+  // Don't trigger if user is typing in an input field
+  if (e.target.matches('input, textarea')) return;
+  
+  const key = e.key.toLowerCase();
+  
+  switch(key) {
+    case 'b':
+      window.location.href = '/';
+      break;
+    case 'a':
+      window.location.href = '/about.html';
+      break;
+    case 'r':
+      window.open('/assets/resume.pdf', '_blank');
+      break;
+    case 'g':
+      window.open('https://github.com/haritha-j', '_blank');
+      break;
+  }
+});
